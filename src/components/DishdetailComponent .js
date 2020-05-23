e9fc0ffeebc695703e1216fc9719efb95e4c9c3a
Jigar44 <jigarrabadiya403@gmail.com>
2020-05-24 01:25:20 +0530

    redux thunk

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,
     Modal, ModalHeader, ModalBody, Label, Row } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 
     function RenderDish({dish}){   
@@ -58,32 +59,52 @@ import { Link } from 'react-router-dom';
     }
 
     const DishDetail = (props) => {
-        const dish = props.dish
-        if (dish == null) {
-            return (<div></div>)
-        }
-    
-        return (
+        if(props.isLoading) {
+            return(
                 <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr />
-                    </div>                
-                </div>
-                <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments}
-                        addComment={props.addComment}
-                        dishId={props.dish.id}
-                    />
-                </div>
+                    <div className="row">
+                        <Loading />
+                    </div>
                 </div>
             );
+        }else if(props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }else if (props.dish != null) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id}
+                        />
+                    </div>
+                </div>
+            );
+        }else{
+            return (
+                <div>
+                </div>
+            );
+        }
+        
+        
     }
 
     
@@ -100,8 +121,8 @@ import { Link } from 'react-router-dom';
                 author: '',
                 comment: '',
                 touched: {
-                    comment: '',
-                    author: '',
+                    comment: false,
+                    author:false,
                 },
                 isModalOpen: false
             }
